4d6518d83a24eb8b52146a0aa2f13a302b2566a4
aliniko <aliverchan@gmail.com>
2021-09-14 22:02:18 +0200

    formatTime removed the comma and fixed it fixed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function App() {
   const [displayTime, setDisplayTime] = React.useState(25 * 60);
 
+  // const []
+
   function formatTime(time) {
     let minutes = Math.floor(time / 60);
     let seconds = time % 60;
@@ -17,7 +19,7 @@ function App() {
         changeTime={null}
         type={"break"}
         time={null}
-        formatTime={"formatTime"}
+        formatTime={formatTime}
         />
       <h1> {formatTime(displayTime)} </h1>
     </div>
@@ -33,7 +35,8 @@ function Length({ title, changeTime, type, time, formatTime }) {
           <i className="material-icons">arrow_downward</i>
         </button>
         {/* this shows an error "formatTime()" is not a funtion but infact that is already a function, so now I am working on that why it shows such error */}
-        {/* <h3> {formatTime(time)} </h3> */}
+       
+        <h3> {formatTime(time)} </h3>
 
         <button className="btn-small deep-purple lighten-2">
           <i className="material-icons">arrow_upward</i>
