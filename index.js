527e9fc4551c73c6d629a53f25a43d0940860bbf
aliniko <aliverchan@gmail.com>
2021-09-15 18:33:56 +0200

    background img and color

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ function App() {
   const [timerOn, setTimerOn] = React.useState(false);
   const [onBreak, setOnBreak] = React.useState(false);
   const [breakAudio, setBreakAudio] = React.useState(
-    new Audio("./sound/beep-01a.mp3"));
-
+    new Audio("./sound/beep-01a.mp3")
+  );
 
   const playBreakSound = () => {
-    breakAudio.currentTime = 0
-    breakAudio.play()
-  }
-
+    breakAudio.currentTime = 0;
+    breakAudio.play();
+  };
 
   function formatTime(time) {
     let minutes = Math.floor(time / 60);
@@ -43,49 +42,48 @@ function App() {
 
   const controlTime = () => {
     let second = 1000;
-    let date = new Date().getTime()
-    let nextDate = new Date().getTime() + second 
-    let onBreakVariable = onBreak
-    if(!timerOn){
+    let date = new Date().getTime();
+    let nextDate = new Date().getTime() + second;
+    let onBreakVariable = onBreak;
+    if (!timerOn) {
       let interval = setInterval(() => {
-        date = new Date().getTime()
-        if(date > nextDate){
+        date = new Date().getTime();
+        if (date > nextDate) {
           setDisplayTime((prev) => {
-              if(prev <= 0 && !onBreakVariable){
-                playBreakSound()
-                onBreakVariable = true
-                setOnBreak(true)
-                return breakTime
-              }else if(prev <= 0 && onBreakVariable) {
-                playBreakSound()
-                onBreakVariable = false
-                setOnBreak(false)
-                return sessionTime
-              }
-            return prev - 1
-          })
-          nextDate += second
+            if (prev <= 0 && !onBreakVariable) {
+              playBreakSound();
+              onBreakVariable = true;
+              setOnBreak(true);
+              return breakTime;
+            } else if (prev <= 0 && onBreakVariable) {
+              playBreakSound();
+              onBreakVariable = false;
+              setOnBreak(false);
+              return sessionTime;
+            }
+            return prev - 1;
+          });
+          nextDate += second;
         }
       }, 30);
-      localStorage.clear()
-      localStorage.setItem("interval-id", interval)
+      localStorage.clear();
+      localStorage.setItem("interval-id", interval);
     }
 
-    if(timerOn){
-      clearInterval(localStorage.getItem("interval-id"))
+    if (timerOn) {
+      clearInterval(localStorage.getItem("interval-id"));
     }
-    setTimerOn(!timerOn)
-
-  }
+    setTimerOn(!timerOn);
+  };
 
   const resetTime = () => {
-    setDisplayTime(25 * 60)
-    setBreakTime(5 * 60)
-    setSessionTime(25 * 60)
-  }
+    setDisplayTime(25 * 60);
+    setBreakTime(5 * 60);
+    setSessionTime(25 * 60);
+  };
 
   return (
-    <div className="center-align">
+    <div className="center-align displayTime">
       <h1>25 + 5 Clock / Pomodoro Clock</h1>
 
       <div className="dual-container">
@@ -105,21 +103,27 @@ function App() {
           formatTime={formatTime}
         />
       </div>
+      <div className=" center-align">
         <h3>{onBreak ? "Break" : "Session"} </h3>
         <h1> {formatTime(displayTime)} </h1>
 
-        <button className="btn-large deep-purple lighten-2" onClick={controlTime}>
+        <button
+          className="btn-medium deep-purple lighten-2"
+          onClick={controlTime}
+        >
           {timerOn ? (
             <i className="material-icons">pause_circle_filled</i>
           ) : (
             <i className="material-icons">play_circle_filled</i>
-
           )}
         </button>
-            <button className="btn-large deep-purple lighten-2" onClick={resetTime}>
-            <i className="material-icons">autorenew</i>
-
-            </button>
+        <button
+          className="btn-medium deep-purple lighten-2"
+          onClick={resetTime}
+        >
+          <i className="material-icons">autorenew</i>
+        </button>
+      </div>
     </div>
   );
 }
